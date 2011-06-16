90a202167003adc8342bbd65653b95aee96c4b82
Justin Noel <justin.noel@syniverse.com>
2011-06-15 19:31:43 -0500

    Modified the php output to count the number of records returned.
    Signed-off-by: Justin Noel <justin.noel@syniverse.com>

--- customer_list.php
+++ customer_list.php
@@ -4,14 +4,16 @@
 $customer_list = array();
 
 $customer_list['success']		= true;
-$customer_list['totalCount']	= 500;
 
 $customers[] = array('customers_id' => 1, 'customer_name' => 'Janet Moore',	 'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
 $customers[] = array('customers_id' => 2, 'customer_name' => 'Jane Doe',	 'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
 $customers[] = array('customers_id' => 3, 'customer_name' => 'Jim Doe',		 'start_date' => '2010-03-19', 'last_order' => '2010-03-19');
 $customers[] = array('customers_id' => 4, 'customer_name' => 'Mystery Man',	 'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
 
-$customer_list['customers'] = $customers;
+$customer_list['customers']		= $customers;
+
+$customer_list['totalCount']	= count( $customer_list['customers'] );
+
 
 echo json_encode($customer_list);
 
--- vendor_list.php
+++ vendor_list.php
@@ -4,7 +4,6 @@
 $vendor_list = array();
 
 $vendor_list['success']		= true;
-$vendor_list['totalCount']	= 500;
 
 $vendors[] = array('vendors_id' => 1, 'vendor_name' => 'AAA Plumbing',			'payment_terms' => 'COD',	'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
 $vendors[] = array('vendors_id' => 2, 'vendor_name' => 'Two Men and A Truck',	'payment_terms' => 'COD',	'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
@@ -12,6 +11,8 @@ $vendors[] = array('vendors_id' => 3, 'vendor_name' => 'Dewey Cheatem & How',	'p
 $vendors[] = array('vendors_id' => 4, 'vendor_name' => 'Avery Air Sucks',		'payment_terms' => 'COD',	'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
 
 $vendor_list['vendors'] = $vendors;
+
+$vendor_list['totalCount']	= count( $vendor_list['customers'] );
 
 echo json_encode($vendor_list);
 
