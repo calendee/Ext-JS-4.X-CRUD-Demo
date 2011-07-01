14513ffd1bedfb92b1bc40712bf2744737d879f6
Justin Noel <justin.noel@syniverse.com>
2011-07-01 16:11:40 -0500

    Modified the customers data sources to use sessions for remembering user actions.
    Signed-off-by: Justin Noel <justin.noel@syniverse.com>

--- customer_list.php
+++ customer_list.php
@@ -1,15 +1,24 @@
 <?php
+ob_start();
+session_start();
 
+if( isset($_SESSION['customers']) )
+{
+	$customers = $_SESSION['customers'];
+} else
+	{
+		$customers[] = array('customers_id' => 1, 'customer_name' => 'Avery Air Sucks Wind', 'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
+		$customers[] = array('customers_id' => 2, 'customer_name' => 'Jane Doe',	 'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
+		$customers[] = array('customers_id' => 3, 'customer_name' => 'Jim Doe',		 'start_date' => '2010-03-19', 'last_order' => '2010-03-19');
+		$customers[] = array('customers_id' => 4, 'customer_name' => 'Mystery Man',	 'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
+		
+		$_SESSION['customers'] = $customers;
+		$_SESSION['max_customers_id'] = 4;
+		
+	}
 
 $customer_list = array();
-
 $customer_list['success']		= true;
-
-$customers[] = array('customers_id' => 1, 'customer_name' => 'Janet Moore',	 'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
-$customers[] = array('customers_id' => 2, 'customer_name' => 'Jane Doe',	 'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
-$customers[] = array('customers_id' => 3, 'customer_name' => 'Jim Doe',		 'start_date' => '2010-03-19', 'last_order' => '2010-03-19');
-$customers[] = array('customers_id' => 4, 'customer_name' => 'Mystery Man',	 'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
-
 $customer_list['customers']		= $customers;
 
 $customer_list['totalCount']	= count( $customer_list['customers'] );
@@ -17,4 +26,5 @@ $customer_list['totalCount']	= count( $customer_list['customers'] );
 
 echo json_encode($customer_list);
 
+ob_flush();
 ?>
--- customers.php
+++ customers.php
@@ -1,16 +1,10 @@
 <?php
-
-
+ob_start();
+session_start();
 
 date_default_timezone_set('America/Chicago');
 
 $form_data = json_decode(file_get_contents('php://input'));
-
-$response['customer']['customer_name'] = $form_data->customer_name;
-$response['customer']['start_date'] = date('Y-m-d');
-$response['customer']['customers_id'] = 23;
-$response['customer']['success'] = true;
-
 
 if( $form_data->customer_name == 'Test Customer')
 {
@@ -18,6 +12,16 @@ if( $form_data->customer_name == 'Test Customer')
 	exit;
 }
 
+
+$response['customer']['customer_name'] = $form_data->customer_name;
+$response['customer']['start_date'] = date('Y-m-d');
+$response['customer']['customers_id'] = $_SESSION['max_customers_id'] + 1;
+$response['customer']['success'] = true;
+
+$_SESSION['customers'][] = $response['customer'];
+
+
 echo json_encode($response);
 
+ob_flush();
 ?>
