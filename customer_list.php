e39eb7a882fa93248fd4d334bb8b35fbce19282b
Justin Noel <justin.noel@syniverse.com>
2011-07-01 16:16:58 -0500

    Updated customer names to sync with edit form.
    Signed-off-by: Justin Noel <justin.noel@syniverse.com>

--- customer_list.php
+++ customer_list.php
@@ -7,10 +7,10 @@ if( isset($_SESSION['customers']) )
 	$customers = $_SESSION['customers'];
 } else
 	{
-		$customers[] = array('customers_id' => 1, 'customer_name' => 'Avery Air Sucks Wind', 'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
-		$customers[] = array('customers_id' => 2, 'customer_name' => 'Jane Doe',	 'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
-		$customers[] = array('customers_id' => 3, 'customer_name' => 'Jim Doe',		 'start_date' => '2010-03-19', 'last_order' => '2010-03-19');
-		$customers[] = array('customers_id' => 4, 'customer_name' => 'Mystery Man',	 'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
+		$customers[] = array('customers_id' => 1, 'customer_name' => 'Avery Airconditioning Sucks Wind', 'start_date' => '2011-05-01', 'last_order' => '2011-05-01');
+		$customers[] = array('customers_id' => 2, 'customer_name' => 'Trial N Error',	 'start_date' => '2009-11-21', 'last_order' => '2010-12-18');
+		$customers[] = array('customers_id' => 3, 'customer_name' => 'In N Out Burger',		 'start_date' => '2010-03-19', 'last_order' => '2010-03-19');
+		$customers[] = array('customers_id' => 4, 'customer_name' => 'Lousy Co.',	 'start_date' => '1998-10-03', 'last_order' => '2011-06-21');
 		
 		$_SESSION['customers'] = $customers;
 		$_SESSION['max_customers_id'] = 4;
